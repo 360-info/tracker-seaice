f35630d5776393968f2d94149a3d412858e83428
James Goldie <james.goldie@360info.org>
2024-01-17 15:20:43 +1100

    Finish ingest code

--- R/check.r
+++ R/check.r
@@ -1,18 +1,93 @@
-#!/usr/bin/env Rscript
-
+library(httr2)
+library(lubridate)
 library(here)
 source(here("R", "util.r"))
 
-# check whether obs are stale based on the date-time of the last update
-north_is_stale <- check_remote_obs_stale("north")
-south_is_stale <- check_remote_obs_stale("south")
+# check the latest update time from noaa
 
-stopifnot(
-  "Error: check_remote_obs_stale() returned a missing value." =
-    !is.na(is_stale))
+sea_ice_url("south") |>
+  request() |>
+  req_method("HEAD") |>
+  req_perform() ->
+req_south
 
-# save whether obs are stale to env var $MONTHLY_IS_STALE for later steps
-message("Are northern observations stale? ", north_is_stale)
-message("Are southern observations stale? ", south_is_stale)
-write_to_gha_env("NORTH_IS_STALE", north_is_stale)
-write_to_gha_env("SOUTH_IS_STALE", south_is_stale)
+sea_ice_url("north") |>
+  request() |>
+  req_method("HEAD") |>
+  req_perform() ->
+req_north
+
+# some quick error-handling
+
+if (resp_is_error(req_north) || resp_is_error(req_north)) {
+  stop("NOAA server unavailable for one or both sea ice data sources.")
+}
+
+if (
+  !resp_header_exists(req_north, "Last-Modified") ||
+  !resp_header_exists(req_south, "Last-Modified")) {
+  stop("Cannot check update time for one or both NOAA sea ice data sources.")
+}
+
+# get the update date-times from the requests
+# (they come in as GMT; we'll convert to UTC internally)
+resp_header(req_north, "Last-Modified") |>
+  parse_date_time("admYHMS") |>
+  force_tz("GMT") ->
+north_update_dt
+
+resp_header(req_south, "Last-Modified") |>
+  parse_date_time("admYHMS") |>
+  force_tz("GMT") ->
+south_update_dt
+
+# compare against previous update times
+
+last_update_path_north <- here("data", "last-monthly-update-north.txt")
+last_update_path_south <- here("data", "last-monthly-update-south.txt")
+
+# if either previous update time is missing,
+# write out the new values and mark as stale
+if (
+  !file.exists(last_update_path_north) ||
+  !file.exists(last_update_path_south)) {
+
+  north_update_dt |>
+    with_tz("UTC") |>
+    as.character() |>
+    writeLines(last_update_path_north)
+  south_update_dt |>
+    with_tz("UTC") |>
+    as.character() |>
+    writeLines(last_update_path_south)
+  write_to_gha_env("DAILY_IS_STALE", "true")
+} else {
+
+  # otherwise, compare the times
+
+  last_update_path_north |>
+    readLines() |>
+    ymd_hms(tz = "UTC") ->
+  old_dt_north
+
+  last_update_path_south |>
+    readLines() |>
+    ymd_hms(tz = "UTC") ->
+  old_dt_south
+
+  if (north_update_dt > old_dt_north || south_update_dt > old_dt_south) {
+    # obs are out of date: write new update times and mark as stale
+    north_update_dt |>
+      with_tz("UTC") |>
+      as.character() |>
+      writeLines(last_update_path_north)
+    south_update_dt |>
+      with_tz("UTC") |>
+      as.character() |>
+      writeLines(last_update_path_south)
+    write_to_gha_env("DAILY_IS_STALE", "true")
+  } else {
+    message("Local sources are still up to date")
+    write_to_gha_env("DAILY_IS_STALE", "false")
+  }
+}
--- R/ingest.r
+++ R/ingest.r
@@ -2,14 +2,7 @@
 
 library(tidyverse)
 library(here)
-
-sea_ice_url <- function(hemisphere = c("north", "south")) {
-  paste0(
-    "https://noaadata.apps.nsidc.org/NOAA/G02135/", hemisphere,
-    "/daily/data/",
-    toupper(substr(hemisphere, 1, 1)),
-    "_seaice_extent_daily_v3.0.csv")
-}
+source(here("R", "util.r"))
 
 # download and tidy the daily obs
 c(sea_ice_url("south"), sea_ice_url("north")) |>
@@ -26,10 +19,11 @@ c(sea_ice_url("south"), sea_ice_url("north")) |>
         "/daily/data" = "")),
     date = ymd(paste(year, month, day)),
     nday = date - ymd(paste(year, "01", "01"))) |>
-  select(hemisphere, date, nday, extent_m_sq_km) ->
+  select(hemisphere, date, nday, extent_m_sq_km) |>
+  arrange(hemisphere, date) ->
 sea_ice
 
-# write dailies out to disk
+# otherwise, write dailies out to disk
 sea_ice |>
   filter(hemisphere == "north") |>
   write_csv(here("data", "seaice-daily-north.csv"))
@@ -69,16 +63,31 @@ prev_lowest_year
 sea_ice |>
   filter(
     hemisphere == "north",
-    year(date) == (prev_lowest_year |>
-      filter(hemisphere == "north") |>
-      pull(year))) |>
+    year(date) ==
+      (prev_lowest_year |> filter(hemisphere == "north") |> pull(year))) |>
   write_csv(here("data", "seaice-lowestyear-north.csv"))
 
 sea_ice |>
   filter(
     hemisphere == "south",
-    year(date) == (prev_lowest_year |>
-      filter(hemisphere == "south") |>
-      pull(year))) |>
+    year(date) ==
+      (prev_lowest_year |> filter(hemisphere == "south") |> pull(year))) |>
   write_csv(here("data", "seaice-lowestyear-south.csv"))
 
+# finally, write out this year
+sea_ice |>
+  filter(
+    hemisphere == "north",
+    year(date) == substr(Sys.Date(), 1, 4)) |>
+  write_csv(here("data", "seaice-thisyear-north.csv"))
+
+sea_ice |>
+  filter(
+    hemisphere == "south",
+    year(date) == substr(Sys.Date(), 1, 4)) |>
+  write_csv(here("data", "seaice-thisyear-south.csv"))
+
+# record update time for subsequent steps (basically to insert into slack msg)
+write_to_gha_env("DAILY_RUN", "true")
+write_to_gha_env("DAILY_RUN_END", Sys.time())
+message("Successfully updated!")
